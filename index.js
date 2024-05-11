11bb3eacfa57e8b2d52ac2f6b1bd2c05283fab8e
Rakesh Biswas <rbiswas01999@gmail.com>
2024-05-11 13:35:31 +0600

    DB modified

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uuibjb3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
+// console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,6 +30,7 @@ async function run() {
         await client.connect();
 
         const bookCollection = client.db('bookDB').collection('book');
+        const borrowedBookCollection = client.db('bookDB').collection('borrowedBook');
         const categoryCollection = client.db('bookDB').collection('category');
 
 
@@ -61,7 +62,7 @@ async function run() {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const options = { upsert: true };
-            // const updateSpot = req.body;
+            // const updateBook = req.body;
             const updateBook = req.body;
 
             const book = {
@@ -70,7 +71,7 @@ async function run() {
                     photo: updateBook.photo,
                     shortDescription: updateBook.shortDescription,
                     authorName: updateBook.authorName,
-                    quantityBook: updateBook.quantityBook,
+                    // quantityBook: updateBook.quantityBook,
                     category: updateBook.category,
                     rating: updateBook.rating,
                     contents: updateBook.contents
@@ -82,11 +83,160 @@ async function run() {
             res.send(result);
         })
 
+        // ------------- update decrement ----------
+
+        // Decrement the quantityBook field by 1
+        app.put('/book/decrement/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const filter = { _id: new ObjectId(id) };
+                const updateBook = req.body;
+
+                // const quantityBook = parseInt(quantityBook);
+                const update = {
+                    $inc: {
+                        quantityBook: -1,
+                    }
+                };
+
+                // Convert quantityBook field to a numeric type using parseInt
+                update.$inc.quantityBook = parseInt(update.$inc.quantityBook);
+
+                console.log(updateBook);
+                const result = await bookCollection.updateOne(filter, update);
+                res.send(result);
+            } catch (error) {
+                console.error('Error incrementing quantityBook:', error);
+                res.status(500).send({ error: 'Internal server error' });
+            }
+        });
+
+
+        // ------------ update increment ------------
+        // Increment the quantityBook field by 1
+        app.put('/book/increment/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const filter = { _id: new ObjectId(id) };
+                const updateBook = req.body;
+
+                // const quantityBook = parseInt(quantityBook);
+                const update = {
+                    $inc: {
+                        quantityBook: 1,
+                    }
+                };
+
+                // Convert quantityBook field to a numeric type using parseInt
+                // update.$inc.quantityBook = parseInt(update.$inc.quantityBook);
+
+                console.log(updateBook);
+                const result = await bookCollection.updateOne(filter, update);
+                res.send(result);
+            } catch (error) {
+                console.error('Error incrementing quantityBook:', error);
+                res.status(500).send({ error: 'Internal server error' });
+            }
+        });
+
+        // ----------- updt another ---------
+        // app.put('/book/decrement/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const filter = { _id: new ObjectId(id) };
+        //     // const options = { upsert: true };
+        //     const updateBook = req.body;
+        //     // Construct the update operation with $inc operator for quantityBook field
+        //     const updateOperation = {
+        //         $inc: {
+        //             quantityBook: -1 // Decrement quantityBook by 1
+        //         },
+        //         $set: {
+        //             bookName: updateBook.bookName,
+        //             photo: updateBook.photo,
+        //             shortDescription: updateBook.shortDescription,
+        //             authorName: updateBook.authorName,
+        //             // quantityBook: updateBook.quantityBook,
+        //             category: updateBook.category,
+        //             rating: updateBook.rating,
+        //             contents: updateBook.contents
+        //         }
+        //     };
+
+        //     try {
+        //         const result = await bookCollection.updateOne(filter, updateOperation);
+        //         res.send(result);
+        //     } catch (error) {
+        //         console.error("Error decrementing book quantity:", error);
+        //         res.status(500).send("Error decrementing book quantity");
+        //     }
+        // });
+
+
 
         app.delete('/book/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await bookCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
+        // ------------- borrowedBook -----------
+        app.get('/borrowedBook', async (req, res) => {
+            const cursor = borrowedBookCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/borrowedBook/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await borrowedBookCollection.findOne(query);
+            res.send(result);
+        })
+
+
+        app.post('/borrowedBook', async (req, res) => {
+            const newBorrowedBook = req.body;
+            console.log(newBorrowedBook);
+            const result = await borrowedBookCollection.insertOne(newBorrowedBook);
+            res.send(result);
+        })
+
+        app.put('/borrowedBook/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            // const updateSpot = req.body;
+            const updateBorrowedBook = req.body;
+
+            const borrowedBook = {
+                $set: {
+                    bookName: updateBorrowedBook.bookName,
+                    bookId: updateBorrowedBook.bookId,
+                    photo: updateBorrowedBook.photo,
+                    shortDescription: updateBorrowedBook.shortDescription,
+                    authorName: updateBorrowedBook.authorName,
+                    // quantityBook: updateBorrowedBook.quantityBook,
+                    category: updateBorrowedBook.category,
+                    rating: updateBorrowedBook.rating,
+                    contents: updateBorrowedBook.contents
+
+                }
+            }
+
+
+
+            const result = await borrowedBookCollection.updateOne(filter, borrowedBook, options);
+            res.send(result);
+        })
+
+
+        app.delete('/borrowedBook/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await borrowedBookCollection.deleteOne(query);
             res.send(result);
         })
 
@@ -97,22 +247,22 @@ async function run() {
             const cursor = categoryCollection.find();
             const result = await cursor.toArray();
             res.send(result);
-          })
-      
-          app.get('/category/:id', async (req, res) => {
+        })
+
+        app.get('/category/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await categoryCollection.findOne(query);
             res.send(result);
-          })
-      
-          app.post('/category', async (req, res) => {
+        })
+
+        app.post('/category', async (req, res) => {
             // const newCategory = req.body;
             const category = req.body;
             console.log(category);
             const result = await categoryCollection.insertOne(category);
             res.send(result);
-          })
+        })
 
 
         // Send a ping to confirm a successful connection
