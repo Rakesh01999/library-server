cdeedb57ba7eb14228fc2dd149200356f91a623d
Rakesh Biswas <rbiswas01999@gmail.com>
2024-05-12 12:56:28 +0600

    lst added

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -53,6 +58,7 @@ async function run() {
 
         app.get('/book', async (req, res) => {
             const cursor = bookCollection.find();
+            console.log('TOKEN ::: ', req.cookies.token);
             const result = await cursor.toArray();
             res.send(result);
         })
